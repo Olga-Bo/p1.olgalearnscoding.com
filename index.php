34d98b0701b4a85e5f9172add609201b8fc50178
Olga-Bo <bolkunova.o@gmail.com>
2013-09-16 13:06:47 -0400

    Added index page and css folder

---
+++ index.php
@@ -0,0 +1,28 @@
+
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <title></title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width">
+
+        <!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
+
+    <!-- Bootstrap -->
+        <link href="css/bootstrap.min.css" rel="stylesheet" media="screen">
+        <link rel="stylesheet" href="css/main.css">
+                              
+    </head>
+    <body>
+    	<div class="container">
+    		<h1>Olga Bolkunova</h1>
+
+    		<p>I am an entrepreneur and Chinese teacher. I was managing partnershiprs with Chinese Universities at Fareastern Federal University (Vladivostok, Russia) before moving to the US to persue Masters degree in Global Studies and International Affairs. I 've started coding two months ago, and so far have created my online portfolio at <a href="http://www.olgabo.com">olgabo.com</a>. </p>
+
+    		<h4>My code editor is Sublime Text.</h4> 
+
+    	</div>
+
+    </body>
+    </html>
